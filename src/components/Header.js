6332c107de8c7ebfeac777af785a4f875b518354
Isaac Miti <ikayz@live.com>
2019-04-09 00:13:03 +0200

    Add styles to header component and prop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
+  const { textStyle, viewStyle} = styles;
   return (
-    <View>
-      <Text></Text>
+    <View styles={viewStyle}>
+      <Text styles={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#069',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 12,
+    elevation: 6,
+    color: '#fff'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+}
 
 export default Header;
