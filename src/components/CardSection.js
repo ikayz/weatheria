bc5d5c4a229f15a4ae44556a827bfbd6d5c0c73d
ikayz <ikayz@live.com>
2019-04-23 14:06:17 +0200

    Add Card section and style it

---
+++ src/components/CardSection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardSection = (props) => {
+    return (
+        <View style={styles.cardSectionStyles}>
+            {PaymentResponse.children}
+        </View>
+    );
+};
+
+const styles = {
+    cardSectionStyles: {
+        borderBottomWidth: 1,
+        padding: 5,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderColor: '#fff',
+        position: 'relative'
+    }
+};
+
+export default CardSection;
