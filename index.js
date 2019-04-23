aef18363fac4465c1b4685440c87375772dfdcec
Isaac Miti <ikayz@live.com>
2019-04-24 00:12:12 +0200

    Fixed error causing app to crash with incoming branch change

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ import React from 'react';
 import { AppRegistry, View,Text } from 'react-native';
 import Header from './src/components/Header';
 import Weather from './src/components/Weather';
-<<<<<<< HEAD
-import Search from './src/components/Search';
-=======
->>>>>>> 851c0
 
 const App = () => {
     return (
       <View>
         <Header headerText={'Weatharia'}/>
         <Weather />
-<<<<<<< HEAD
-        {/* <Search placeholder='Search City'/> */}
-=======
->>>>>>> 851c0
       </View>
     );
 };
