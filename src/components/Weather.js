790f1f221a50e5f4c5e66169d0c5f82408cfa69b
Isaac Miti <ikayz@live.com>
2019-04-23 07:08:08 +0200

    Weather api working

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,26 +4,30 @@ import axios from 'axios';
 // import OPENWEATHER_API from '../config';
 
 class Weather extends Component {
-  state = { weather: [], name: '' };
+  state = { weather: [], city: '', };
 
   componentDidMount() {
     axios.get('http://api.openweathermap.org/data/2.5/weather?q=Berlin&APPID=dc6267c2fdea4f59377f61f4b6a2c91a')
       .then(response => {
-        const weather = response.data.weather.map(obj => ({ name: obj.name, description: obj.description }));
-        this.setState({ weather });
+        this.setState({ city: response.data.name, weather: response.data.weather });
+      })
+      .catch(error => {
+        console.log(error);
       });
   }
 
-  renderWeather() {
-    return this.state.weather.map((weather, index) => ( <Text key={index}>{weather.description}</Text>));
-  }
+  // renderWeather() {
+  //   return this.state.weather.map((weather, index) => ( <Text key={index}>{weather.description}</Text>));
+  // }
 
   render() {
-    console.log(this.state);
+    // console.log(this.state);
+    const { city, weather } = this.state;
     return (
       <View>
-        {this.renderWeather()}
-        <Text>Test</Text>
+        {/* {this.renderWeather()} */}
+        <Text>{city}</Text>
+        <Text>{weather.description}</Text>
       </View>
     );
   }
