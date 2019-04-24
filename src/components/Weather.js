4639c234cb1a6eae76d98cc1d871948646aaa274
Isaac Miti <ikayz@live.com>
2019-04-24 22:06:26 +0200

    Add geolocation

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import WeatherDetails from './WeatherDetails';
 
 class Weather extends Component {
-  state = { forecast: [], location: '' };
+  state = { forecast: [], latitude: 0, longitude: 0, error: '' };
 
   componentDidMount() {
     axios.get('http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&units=metric&appid=dc6267c2fdea4f59377f61f4b6a2c91a')
@@ -14,6 +14,22 @@ class Weather extends Component {
       .catch(error => {
         console.log(error);
       });
+  }
+
+  findLocation() {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.setState(
+            (preState) => ({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude
+            }), () => { this.renderWeather(); }
+          );
+        },
+
+        (error) => this.setState({ forecast: error.message }),
+        { enableHighAccuracy: true, timeout: 22000, maximumAge: 1000 }
+      );
   }
 
   renderWeather() {
